6d16135d278894673936bc9ac6deb2294c7a7a10
tobias-indico <tt@indicosys.com>
2021-09-24 14:02:08 +0200

    changes

---
+++ app/src/main/java/com/dji/sdk/sample/demo/mobileremotecontroller/MobileRemoteControllerView.java
@@ -0,0 +1,368 @@
+package com.dji.sdk.sample.demo.mobileremotecontroller;
+
+import android.app.Service;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+
+import com.dji.sdk.sample.R;
+import com.dji.sdk.sample.internal.OnScreenJoystickListener;
+import com.dji.sdk.sample.internal.controller.DJISampleApplication;
+import com.dji.sdk.sample.internal.utils.DialogUtils;
+import com.dji.sdk.sample.internal.utils.ModuleVerificationUtil;
+import com.dji.sdk.sample.internal.utils.OnScreenJoystick;
+import com.dji.sdk.sample.internal.utils.ToastUtils;
+import com.dji.sdk.sample.internal.view.PresentableView;
+
+import java.util.concurrent.TimeUnit;
+
+import dji.common.camera.SettingsDefinitions;
+import dji.common.error.DJIError;
+import dji.common.flightcontroller.simulator.InitializationData;
+import dji.common.flightcontroller.simulator.SimulatorState;
+import dji.common.model.LocationCoordinate2D;
+import dji.common.util.CommonCallbacks;
+import dji.common.util.CommonCallbacks.CompletionCallback;
+import dji.keysdk.FlightControllerKey;
+import dji.keysdk.KeyManager;
+import dji.sdk.camera.Camera;
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.flightcontroller.Simulator;
+import dji.sdk.media.MediaFile;
+import dji.sdk.mobilerc.MobileRemoteController;
+import dji.sdk.products.Aircraft;
+
+/**
+ * Class for mobile remote controller.
+ */
+public class MobileRemoteControllerView extends RelativeLayout
+        implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, PresentableView {
+
+    private ToggleButton btnSimulator;
+    private Button btnTakeOff;
+    private Button autoLand;
+    private Button forceLand;
+    private Button indicoMissionBtn;
+
+    private TextView textView;
+
+    private OnScreenJoystick screenJoystickRight;
+    private OnScreenJoystick screenJoystickLeft;
+    private MobileRemoteController mobileRemoteController;
+    private FlightControllerKey isSimulatorActived;
+
+    public MobileRemoteControllerView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    @NonNull
+    @Override
+    public String getHint() {
+        return this.getClass().getSimpleName() + ".java";
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        setUpListeners();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        tearDownListeners();
+        super.onDetachedFromWindow();
+    }
+
+    private void init(Context context) {
+        setClickable(true);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.view_mobile_rc, this, true);
+        initAllKeys();
+        initUI();
+    }
+
+    private void initAllKeys() {
+        isSimulatorActived = FlightControllerKey.create(FlightControllerKey.IS_SIMULATOR_ACTIVE);
+    }
+
+    private void initUI() {
+        btnTakeOff = (Button) findViewById(R.id.btn_take_off);
+        autoLand = (Button) findViewById(R.id.btn_auto_land);
+        autoLand.setOnClickListener(this);
+        indicoMissionBtn = (Button) findViewById(R.id.btn_indico_mission);
+        indicoMissionBtn.setOnClickListener(this);
+//        forceLand = (Button) findViewById(R.id.btn_force_land);
+//        forceLand.setOnClickListener(this);
+        btnSimulator = (ToggleButton) findViewById(R.id.btn_start_simulator);
+
+        textView = (TextView) findViewById(R.id.textview_simulator);
+
+        screenJoystickRight = (OnScreenJoystick) findViewById(R.id.directionJoystickRight);
+        screenJoystickLeft = (OnScreenJoystick) findViewById(R.id.directionJoystickLeft);
+
+        btnTakeOff.setOnClickListener(this);
+        btnSimulator.setOnCheckedChangeListener(MobileRemoteControllerView.this);
+
+        Boolean isSimulatorOn = (Boolean) KeyManager.getInstance().getValue(isSimulatorActived);
+        if (isSimulatorOn != null && isSimulatorOn) {
+            btnSimulator.setChecked(true);
+            textView.setText("Simulator is On.");
+        }
+    }
+
+    private void setUpListeners() {
+        Simulator simulator = ModuleVerificationUtil.getSimulator();
+        if (simulator != null) {
+            simulator.setStateCallback(new SimulatorState.Callback() {
+                @Override
+                public void onUpdate(final SimulatorState djiSimulatorStateData) {
+                    ToastUtils.setResultToText(textView,
+                            "Yaw : "
+                                    + djiSimulatorStateData.getYaw()
+                                    + ","
+                                    + "X : "
+                                    + djiSimulatorStateData.getPositionX()
+                                    + "\n"
+                                    + "Y : "
+                                    + djiSimulatorStateData.getPositionY()
+                                    + ","
+                                    + "Z : "
+                                    + djiSimulatorStateData.getPositionZ());
+                }
+            });
+        } else {
+            ToastUtils.setResultToToast("Disconnected!");
+        }
+        try {
+            mobileRemoteController =
+                    ((Aircraft) DJISampleApplication.getAircraftInstance()).getMobileRemoteController();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        if (mobileRemoteController != null) {
+            textView.setText(textView.getText() + "\n" + "Mobile Connected");
+        } else {
+            textView.setText(textView.getText() + "\n" + "Mobile Disconnected");
+        }
+        screenJoystickLeft.setJoystickListener(new OnScreenJoystickListener() {
+
+            @Override
+            public void onTouch(OnScreenJoystick joystick, float pX, float pY) {
+                if (Math.abs(pX) < 0.02) {
+                    pX = 0;
+                }
+
+                if (Math.abs(pY) < 0.02) {
+                    pY = 0;
+                }
+
+                if (mobileRemoteController != null) {
+                    mobileRemoteController.setLeftStickHorizontal(pX);
+                    mobileRemoteController.setLeftStickVertical(pY);
+                }
+            }
+        });
+
+        screenJoystickRight.setJoystickListener(new OnScreenJoystickListener() {
+
+            @Override
+            public void onTouch(OnScreenJoystick joystick, float pX, float pY) {
+                if (Math.abs(pX) < 0.02) {
+                    pX = 0;
+                }
+
+                if (Math.abs(pY) < 0.02) {
+                    pY = 0;
+                }
+                if (mobileRemoteController != null) {
+                    mobileRemoteController.setRightStickHorizontal(pX);
+                    mobileRemoteController.setRightStickVertical(pY);
+                }
+            }
+        });
+    }
+
+    private void tearDownListeners() {
+        Simulator simulator = ModuleVerificationUtil.getSimulator();
+        if (simulator != null) {
+            simulator.setStateCallback(null);
+        }
+        screenJoystickLeft.setJoystickListener(null);
+        screenJoystickRight.setJoystickListener(null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FlightController flightController = ModuleVerificationUtil.getFlightController();
+        if (flightController == null) {
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.btn_indico_mission:
+
+                indicoMission(flightController);
+                break;
+
+            case R.id.btn_take_off:
+
+                flightController.startTakeoff(new CompletionCallback() {
+                    @Override
+                    public void onResult(DJIError djiError) {
+                        DialogUtils.showDialogBasedOnError(getContext(), djiError);
+                    }
+                });
+                break;
+//            case R.id.btn_force_land:
+//                flightController.confirmLanding(new CompletionCallback() {
+//                    @Override
+//                    public void onResult(DJIError djiError) {
+//                        DialogUtils.showDialogBasedOnError(getContext(), djiError);
+//                    }
+//                });
+//                break;
+            case R.id.btn_auto_land:
+                flightController.startLanding(new CompletionCallback() {
+                    @Override
+                    public void onResult(DJIError djiError) {
+                        DialogUtils.showDialogBasedOnError(getContext(), djiError);
+                    }
+                });
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void takePhoto() {
+        try {
+            Log.d("SHOOT_PHOTO_TEST", "Before photo");
+            initCamera();
+
+            DJISampleApplication.getProductInstance()
+                    .getCamera()
+                    .startShootPhoto(new CommonCallbacks.CompletionCallback() {
+                        @Override
+                        public void onResult(DJIError djiError) {
+                            if (null == djiError) {
+                                ToastUtils.setResultToToast(getContext().getString(R.string.success));
+                            } else {
+                                ToastUtils.setResultToToast(djiError.getDescription());
+                            }
+//                            post(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    middleBtn.setEnabled(true);
+//                                }
+//                            });
+                        }
+                    });
+            Log.d("SHOOT_PHOTO_TEST", "After photo");
+        } catch (Exception e) {
+            Log.d("SHOOT_PHOTO_TEST", "Failed: " + e.getMessage());
+        }
+    }
+
+    private void indicoMission(FlightController flightController) {
+        // Take OFF
+        flightController.startTakeoff(new CompletionCallback() {
+
+            @Override
+            public void onResult(DJIError djiError) {
+                DialogUtils.showDialogBasedOnError(getContext(), djiError);
+
+                // Hang around
+                if (null == djiError) {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+
+                    }
+                    // Capture
+                    takePhoto();
+
+                    // wait
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+
+                    }
+
+                    // Auto land
+                    flightController.startLanding(new CompletionCallback() {
+                        @Override
+                        public void onResult(DJIError djiError) {
+                            DialogUtils.showDialogBasedOnError(getContext(), djiError);
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    private void initCamera() {
+        Camera camera;
+        if (ModuleVerificationUtil.isCameraModuleAvailable()) {
+            camera = DJISampleApplication.getAircraftInstance().getCamera();
+            if (ModuleVerificationUtil.isMatrice300RTK() || ModuleVerificationUtil.isMavicAir2()) {
+                camera.setFlatMode(SettingsDefinitions.FlatCameraMode.PHOTO_SINGLE, djiError -> ToastUtils.setResultToToast("setFlatMode to PHOTO_SINGLE"));
+            } else {
+                camera.setMode(SettingsDefinitions.CameraMode.SHOOT_PHOTO, djiError -> ToastUtils.setResultToToast("setMode to shoot_PHOTO"));
+            }
+            camera.setMediaFileCallback(new MediaFile.Callback() {
+                @Override
+                public void onNewFile(@NonNull MediaFile mediaFile) {
+                    ToastUtils.setResultToToast("New photo generated");
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (compoundButton == btnSimulator) {
+            onClickSimulator(b);
+        }
+    }
+
+    private void onClickSimulator(boolean isChecked) {
+        Simulator simulator = ModuleVerificationUtil.getSimulator();
+        if (simulator == null) {
+            return;
+        }
+        if (isChecked) {
+
+            textView.setVisibility(VISIBLE);
+            simulator.start(InitializationData.createInstance(new LocationCoordinate2D(23, 113), 10, 10),
+                    new CompletionCallback() {
+                        @Override
+                        public void onResult(DJIError djiError) {
+
+                        }
+                    });
+        } else {
+
+            textView.setVisibility(INVISIBLE);
+            simulator.stop(new CompletionCallback() {
+                @Override
+                public void onResult(DJIError djiError) {
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getDescription() {
+        return R.string.component_listview_mobile_remote_controller;
+    }
+}
